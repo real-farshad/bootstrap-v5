b9726f6558c251aefb6fd6ed923cd14679a3b5c5
m0hammadr3za <mohammad.reza.gholami.dev@gmail.com>
2021-08-29 21:10:24 +0430

    Gallery page added + custom image preview with js

---
+++ js/gallery.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll("#gallery-img")
+const overlay = document.querySelector("#overlay")
+const imagePreview = document.querySelector("#image-preview")
+
+for (const image of images) {
+    image.addEventListener("click", () => {
+        overlay.classList.remove("d-none")
+        imagePreview.setAttribute("src", image.getAttribute("src"))
+        imagePreview.setAttribute("alt", image.getAttribute("alt"))
+        setTimeout(() => {
+            imagePreview.classList.add("show-img-preview")
+        }, 0)
+    })
+}
+
+overlay.addEventListener("click", () => {
+    overlay.classList.add("d-none")
+    imagePreview.setAttribute("src", "")
+    imagePreview.setAttribute("alt", "")
+    imagePreview.classList.remove("show-img-preview")
+})
